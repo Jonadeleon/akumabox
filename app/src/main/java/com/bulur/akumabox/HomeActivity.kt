d2d9edb72e31753d0c2bb2548fcda4ece7a75b5b
jondeleonsola@gmail.com <acm1ptcactm>
2020-12-22 16:20:24 +0100

    Cambio de icono y de pantalla de carga inicial. Cambio fondos de la app Añadido pantalla de registro/acceso Añadiendo pantalla de informacion de jugador

--- app/src/main/java/com/bulur/akumabox/HomeActivity.kt
+++ app/src/main/java/com/bulur/akumabox/HomeActivity.kt
@@ -2,10 +2,28 @@ package com.bulur.akumabox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.oAuthProvider
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val bundle = intent.extras
+        val email = bundle?.getString("email")
+        setup(email ?: "")
+    }
+
+    private fun setup(email: String){
+        title="Inicio"
+        emailTextView.text = email
+
+        logOutButton.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            onBackPressed()
+        }
     }
 }
